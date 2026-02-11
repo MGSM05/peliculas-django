4ba2be9dc07c4c678ec754653c3d139a76780ff7
Daniel García <danielgm.daw@gmail.com>
2026-02-11 10:14:41 +0100

    Creamos el  modelo Pelicula con campos obligatorios y ForeignKey a User

--- peliculas/models.py
+++ peliculas/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Pelicula(models.Model):
+    titulo = models.CharField(max_length=200)                  # texto corto
+    sinopsis = models.TextField()                              # texto largo
+    genero = models.CharField(max_length=100)
+    
+    # Fecha automática
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    
+    autor = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE,
+        related_name='mis_peliculas'
+    )
+
+    def __str__(self):
+        return self.titulo
+
+    def get_absolute_url(self):
+        return reverse('detalle_pelicula', kwargs={'pk': self.pk})
