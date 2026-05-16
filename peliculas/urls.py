2ac781ab8f1fa103824bb929931deaf29048d132
Mario Gonzalez <gonzalezsanmartinmario@gmail.com>
2026-05-16 11:24:17 +0200

    Configuramos las URLs del proyecto, tanto de app peliculas como de accounts

---
+++ peliculas/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import (
+    VistaListaPeliculas,
+    VistaDetallePelicula,
+    VistaCrearPelicula,
+    VistaActualizarPelicula,
+    VistaEliminarPelicula,
+)
+
+urlpatterns = [
+    path('', VistaListaPeliculas.as_view(), name='lista_peliculas'),
+    path('pelicula/<int:pk>/', VistaDetallePelicula.as_view(), name='detalle_pelicula'),
+    path('pelicula/nueva/', VistaCrearPelicula.as_view(), name='nueva_pelicula'),
+    path('pelicula/<int:pk>/editar/', VistaActualizarPelicula.as_view(), name='editar_pelicula'),
+    path('pelicula/<int:pk>/eliminar/', VistaEliminarPelicula.as_view(), name='eliminar_pelicula'),
+]
